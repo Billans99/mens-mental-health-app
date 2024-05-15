f3ee731a8563ab0551006f4f1eb26b5fedf7ed50
Billans99 <billy.xu99@gmail.com>
2024-05-15 17:28:35 +1000

    Completed nav-bar with React-bootstrap navbar component, routing and logo still required

--- mens-mental-health-app/src/App.jsx
+++ mens-mental-health-app/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// imports
+import Header from './Header.jsx'
+import react, { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// states
+//
+
+
+const App = () => {
+
 
   return (
     <>
- 
+      <Header/>
     </>
   )
 }
---
+++ mens-mental-health-app/src/Header.jsx
@@ -0,0 +1,61 @@
+// TO DO: 
+// App-heading, app logo, navbar
+// Create basic routing for app through react-router-dom
+
+
+// imports
+import React, { useEffect, useState } from 'react'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+
+// states
+//
+
+// functions
+//
+
+
+const Header = () => {
+
+    
+    return (
+        <>
+
+         <Navbar variant={'dark'} expand="lg" className="nav-bar">
+            <Container>
+                <Navbar.Brand href="#home" className="nav-heading">
+                    <img
+                        alt=""
+                        src="/img/logo.svg"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                    Men's Mental Health App
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#statistics">Statistics</Nav.Link>
+                        <Nav.Link href="#members">Members</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                        <NavDropdown title="Support" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">
+                            Mental health support links</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.2">
+                            Crisis support
+                        </NavDropdown.Item>                                  
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+                    
+        </>
+    )
+
+}
+
+export default Header
