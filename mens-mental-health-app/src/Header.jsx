920eb90560057757eb9596467139bb55b083ac68
Billans99 <billy.xu99@gmail.com>
2024-05-17 00:03:30 +1000

    Changed font for all elements, and fixed issue with nav link (support dropdown) not applying margin 20px

--- mens-mental-health-app/src/Header.jsx
+++ mens-mental-health-app/src/Header.jsx
@@ -39,13 +39,14 @@ const Header = () => {
                         <Nav.Link href="#members" className="nav-links">Members</Nav.Link>
                         <Nav.Link href="#about" className="nav-links">About</Nav.Link>
                         <Nav.Link href="#contact" className="nav-links">Contact</Nav.Link>
-                        <NavDropdown title="Support" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">
-                            Mental health support links</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.2">
-                            Crisis support
-                        </NavDropdown.Item>                                  
+                        <NavDropdown title="Support" id="basic-nav-dropdown" className="nav-dropdown-container">
+                            <NavDropdown.Item href="#action/3.1">
+                                Mental health support links
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.2">
+                                Crisis support
+                            </NavDropdown.Item>    
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
