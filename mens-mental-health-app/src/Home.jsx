c738ad5ae853f28cb16609767fe8c7b3a39b8f55
Billans99 <billy.xu99@gmail.com>
2024-06-04 13:27:14 +1000

    updated home page with more styling

--- mens-mental-health-app/src/Home.jsx
+++ mens-mental-health-app/src/Home.jsx
@@ -56,119 +56,126 @@ const Home = () => {
             <h1 className="app-heading">Vital Help</h1>
         </div>
 
-    {/* Home page summary, using a grid describing the value this app can deliver.
-        Each row includes, an img, heading, and paragraph body */}
-    <div className="home-summary-container">
+    
+        <div className="home-flex-container">
 
-      <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            p: 1,
-            m: 1,
-            gap: 0,
-            borderRadius: 1,
-            alignItems: 'center',
-          }}
-        >
-          <Item sx={{
-            bgcolor: 'paper',
-            border: 'none',
-          }}>
-            <img className="home-summary-img" src="/home-summary-img1.png"></img>
-          </Item>
-          <Item sx={{
-            bgcolor: 'paper',
-            border: 'none',
-            
-          }}>
-            <h2 className="home-summary-heading">Reach out to a professional</h2>
-          </Item>
-          <Item sx={{
-            bgcolor: 'paper',
-            border: 'none',
-            marginBottom: '100px',
-          }}>
-            <h6 className="home-summary-body">
-                  Reaching out to a professional can be a great step to recovery.
-            </h6>
-          </Item>
-          
-      </Box>
+          <div className="home-summary-container">
+            <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  flexDirection: 'column',
+                  p: 1,
+                  m: 1,
+                  gap: 0,
+                  borderRadius: 1,
+                  alignItems: 'center',
+                }}
+              >
+                <Item sx={{
+                  bgcolor: 'paper',
+                  border: 'none',
+                }}>
+                  <img className="home-summary-img" src="/home-summary-img1.png"></img>
+                </Item>
+                <Item sx={{
+                  bgcolor: 'paper',
+                  border: 'none',
+                  color: '#e6e6e6',
+                }}>
+                  <h2 className="home-summary-heading">Reach out to a professional</h2>
+                </Item>
+                <Item sx={{
+                  bgcolor: 'paper',
+                  border: 'none',
+                  marginBottom: '100px',
+                  color: '#e6e6e6',
+                }}>
+                  <h6 className="home-summary-body">
+                        Reaching out to a professional can be a great step to recovery.
+                  </h6>
+                </Item>
+            </Box>
 
-      <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            p: 1,
-            m: 1,
-            gap: 0,
-            borderRadius: 1,
-            alignItems: 'center',
-          }}
-        >
-          <Item sx={{
-            bgcolor: 'paper',
-            border: 'none',
-          }}>
-            <img className="home-summary-img" src="/home-summary-img2.png"></img>
-          </Item>
-          <Item sx={{
-            bgcolor: 'paper',
-            border: 'none',
-          }}>
-            <h2 className="home-summary-heading">Have more insight</h2>
-          </Item>
-          <Item sx={{
-            bgcolor: 'paper',
-            border: 'none',
-            marginBottom: '100px',
-          }}>
-            <h6 className="home-summary-body">
-                  Statistical data can give you a greater understanding of the problems you face.
-            </h6>
-          </Item>
-          
-      </Box>
+            <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  flexDirection: 'column',
+                  p: 1,
+                  m: 1,
+                  gap: 0,
+                  borderRadius: 1,
+                  alignItems: 'center',
+                }}
+              >
+                <Item sx={{
+                  bgcolor: 'paper',
+                  border: 'none',
+                }}>
+                  <img className="home-summary-img" src="/home-summary-img2.png"></img>
+                </Item>
+                <Item sx={{
+                  bgcolor: 'paper',
+                  border: 'none',
+                  color: '#e6e6e6',
+                }}>
+                  <h2 className="home-summary-heading">Have more insight</h2>
+                </Item>
+                <Item sx={{
+                  bgcolor: 'paper',
+                  border: 'none',
+                  marginBottom: '100px',
+                  color: '#e6e6e6',
+                }}>
+                  <h6 className="home-summary-body">
+                        Statistical data can give you a greater understanding of the problems you face.
+                  </h6>
+                </Item>
+            </Box>
 
-      <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            p: 1,
-            m: 1,
-            gap: 0,
-            borderRadius: 1,
-            alignItems: 'center',
-          }}
-        >
-          <Item sx={{
-            bgcolor: 'paper',
-            border: 'none',
-          }}>
-            <img className="home-summary-img" src="/home-summary-img3.png"></img>
-          </Item>
-          <Item sx={{
-            bgcolor: 'paper',
-            border: 'none',
-          }}>
-            <h2 className="home-summary-heading">External support</h2>
-          </Item>
-          <Item sx={{
-            bgcolor: 'paper',
-            border: 'none',
-          }}>
-            <h6 className="home-summary-body">
-                  Having a support network can be vital in our journey to recovery in mental health.
-            </h6>
-          </Item>
-          
-      </Box>
-      
-    </div>
+            <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  flexDirection: 'column',
+                  p: 1,
+                  m: 1,
+                  gap: 0,
+                  borderRadius: 1,
+                  alignItems: 'center',
+                }}
+              >
+                <Item sx={{
+                  bgcolor: 'paper',
+                  border: 'none',
+                }}>
+                  <img className="home-summary-img" src="/home-summary-img3.png"></img>
+                </Item>
+                <Item sx={{
+                  bgcolor: 'paper',
+                  border: 'none',
+                  color: '#e6e6e6',
+                }}>
+                  <h2 className="home-summary-heading">External support</h2>
+                </Item>
+                <Item sx={{
+                  bgcolor: 'paper',
+                  border: 'none',
+                  color: '#e6e6e6',
+                }}>
+                  <h6 className="home-summary-body">
+                        Having a support network can be vital in our journey to recovery in mental health.
+                  </h6>
+                </Item>
+            </Box>
+          </div>
+
+          {/* Code the other piece of content you want here */}
+          <p>Hello! testing</p>
+        </div>
+    
+    
       
 
 
